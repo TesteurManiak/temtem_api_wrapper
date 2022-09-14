9f3efd4ed91c8c423b8efd3fb5d93732b3c822e4
Guillaume Roux <rouxguillaume8@gmail.com>
2022-09-14 11:14:17 +0200

    finished tests for getTemtems

--- test/mocks/dio_client.dart
+++ test/mocks/dio_client.dart
@@ -3,16 +3,31 @@ import 'dart:convert';
 import 'package:http_mock_adapter/http_mock_adapter.dart';
 import 'package:temtem_api_wrapper/src/provider/api_client.dart';
 import 'package:temtem_api_wrapper/src/provider/http_provider.dart';
+import 'package:temtem_api_wrapper/temtem_api_wrapper.dart';
 
 import '../utils/fixture_reader.dart';
+
+const _temtemsUrl = '${HttpProvider.baseUrl}/api/temtems';
 
 class MockDioClient extends DioClient {
   MockDioClient() {
     final dioAdapter = DioAdapter(dio: client);
 
     dioAdapter.onGet(
-      '${HttpProvider.baseUrl}/api/temtems?',
+      '$_temtemsUrl?',
       (server) => server.reply(200, jsonDecode(fixture('temtems.json'))),
+    );
+    dioAdapter.onGet(
+      '$_temtemsUrl?names=${Uri.encodeComponent('Ganki,Mimit')}',
+      (server) => server.reply(200, jsonDecode(fixture('temtems_2.json'))),
+    );
+    dioAdapter.onGet(
+      '$_temtemsUrl?expand=${Uri.encodeComponent(ExpandableField.values.map((e) => e.name).join(','))}',
+      (server) => server.reply(200, jsonDecode(fixture('temtems_3.json'))),
+    );
+    dioAdapter.onGet(
+      '$_temtemsUrl?weaknesses=true',
+      (server) => server.reply(200, jsonDecode(fixture('temtems_4.json'))),
     );
   }
 }
